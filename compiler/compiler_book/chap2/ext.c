cd81169a8fc514ea358fe16357f52adeb1259f4f
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2026-04-08 22:14:38 +0900

    feat: ptr_add

--- compiler/compiler_book/chap2/ext.c
+++ compiler/compiler_book/chap2/ext.c
@@ -1,3 +1,10 @@
 int foo() { return 42; }
 int add(int x, int y) { return x + y; }
 int powe(int x, int p) {int xx=x; for(int i=0;i<p-1;i++)x=x*xx;return x;}
+int *alloc3() {
+    static int buf[3];
+    buf[0] = 10;
+    buf[1] = 20;
+    buf[2] = 30;
+    return buf;
+}
