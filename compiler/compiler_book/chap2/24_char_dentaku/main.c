cd67d810858b3efcaa2188d008d1306d11ff3bb7
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2026-04-10 21:13:45 +0900

    feat: char

---
+++ compiler/compiler_book/chap2/24_char_dentaku/main.c
@@ -0,0 +1,32 @@
+#include "dentaku.h"
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    if (argc != 2){
+        fprintf(stderr, "引数の数が正しくありません");
+        return 1;
+    }
+
+    user_input = argv[1];
+    tokenize();
+
+    printf(".intel_syntax noprefix\n");
+    program();
+
+    printf(".data\n");
+    for(GVar *g = globals; g; g=g->next){
+        printf(".globl %.*s\n", g->len, g->name);
+        printf("%.*s:\n", g->len, g->name);
+        printf("  .zero %d\n", size_of(g->ty));
+    }
+    printf(".text\n");
+
+    for(int i=0; funcs[i]; i++){
+        add_type(funcs[i]->body, true);
+        gen_func(funcs[i]);
+    }
+
+    printf(".section .note.GNU-stack,\"\",@progbits\n");
+
+    return 0;
+}
