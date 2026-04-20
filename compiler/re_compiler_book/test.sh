6b91af59dc2641c2bb596d5685ab41a0420d73ce
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2026-04-20 20:06:54 +0900

    func_def -> deref_addr

--- compiler/re_compiler_book/test.sh
+++ compiler/re_compiler_book/test.sh
@@ -7,7 +7,8 @@
 #input="a=1; i=4; for(a=1; a<10; a=a+1) {i=i+a; i=i+1;} return i;"
 #input="a = foo(1,2); return a;"
 #input="adfoo(a,b){c=2; return a+b+c;} main(){a = adfoo(1,2); return a;}"
-input="main(){a = 1; b = &a; *b=5; return a;}"
+#input="main(){a = 1; b = &a; *b=5; return a;}"
+input="main(){a = 1; y=x+1; return y;}"
 ./mini_cc "$input" > temp.s
 
 printf "<asm>\n"
