2d97b6262a53b8a3b278e648cb17d15edb3727ff
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2026-04-20 22:25:21 +0900

    type_def (hard)

--- compiler/re_compiler_book/temp.s
+++ compiler/re_compiler_book/temp.s
@@ -1,4 +1,32 @@
 .intel_syntax noprefix
+.globl set5
+set5:
+  push rbp
+  mov rbp, rsp
+  sub rsp, 208
+  mov [rbp-8], rdi
+  mov rax, rbp
+  sub rax, 8
+  push rax
+  pop rax
+  mov rax, [rax]
+  push rax
+  push 5
+  pop rdi
+  pop rax
+  mov [rax], rdi
+  push rdi
+  pop rax
+  push 0
+  pop rax
+  mov rsp, rbp
+  pop rbp
+  ret
+  pop rax
+  push 0
+  mov rsp, rbp
+  pop rbp
+  ret
 .globl main
 main:
   push rbp
@@ -15,30 +43,12 @@ main:
   mov [rax], rdi
   push rdi
   pop rax
-  push 0
-  pop rax
-  mov rax, rbp
-  sub rax, 16
-  push rax
   mov rax, rbp
   sub rax, 8
   push rax
   pop rdi
-  pop rax
-  mov [rax], rdi
-  push rdi
-  pop rax
-  mov rax, rbp
-  sub rax, 16
+  call set5
   push rax
-  pop rax
-  mov rax, [rax]
-  push rax
-  push 5
-  pop rdi
-  pop rax
-  mov [rax], rdi
-  push rdi
   pop rax
   mov rax, rbp
   sub rax, 8
